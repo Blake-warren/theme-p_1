e82a506b66508494bd53b8081e304e8b0cefb9eb
theo <theo.jaegly90@gmail.com>
2020-11-16 15:26:33 +0100

    ajout du template de résulatat de recherche
    presque parfait reste a trouver comment afficher le terme de recherche

---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+    get_header();
+?>
+<div class="category">
+    <h1>terme de recherche xxxxxxxxxxxxxx</h1>
+    <?php get_template_part('loop'); ?>
+</div>
+<?php
+get_sidebar();
+get_footer();
+?>
